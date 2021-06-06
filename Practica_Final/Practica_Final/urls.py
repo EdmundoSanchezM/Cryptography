a66f2a81caa927079a1d8921381543489e58e9d4
Edmundo Josue Sanchez Mendez <edmundoelpro1@gmail.com>
2021-06-06 01:37:48 -0500

    Practica Final

---
+++ Practica_Final/Practica_Final/urls.py
@@ -0,0 +1,30 @@
+"""proyecto1 URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/2.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from example.views import MainPage, FimaDigitalPage, Firmar, Validar, Todo,TodoInverso
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
+urlpatterns = [
+    path('', MainPage, name='index'),
+    path('hybrid_crypto/', FimaDigitalPage, name='ds'),
+    path('CS/<msgfile>/<pkfile>/<pubkfile>', Todo),
+    path('DV/<msgfile>/<pubkfile>/<pkfile>', TodoInverso),
+    path('Firmar/<msgfile>/<pkfile>', Firmar),
+    path('ValidateFile/<msgfile>/<pkfile>', Validar)
+]
+
+urlpatterns += staticfiles_urlpatterns()
